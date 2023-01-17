047e0ff0132f99aa1d1abce3fef71d6498c1e4d5
MathiTarazonad <vladitarazona@gmail.com>
2023-01-17 14:34:33 -0300

    Added path for level selection

---
+++ src/app/levels/page.jsx
@@ -0,0 +1,14 @@
+import LevelButton from '@/components/LevelButton';
+import React from 'react';
+import levels from '../../data/levels.json';
+
+export default function LevelsMenu() {
+  return (
+    <div className='levels-menu-container'>
+      <h2>Level Selection</h2>
+      <div className='levels-list'>
+        {levels.map(level => <LevelButton key={level.index}>{level.level}</LevelButton>)}
+      </div>
+    </div>
+  )
+}
