8817b548a7ce7f599b0fbd7e7af5bf3b8b9ffc1f
MathiTarazonad <vladitarazona@gmail.com>
2023-01-17 14:33:42 -0300

    Created initial CompletedDifficulties component to show what difficulties user has completed

---
+++ src/components/CompletedDifficulties.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function CompletedDifficulties() {
+  return (
+    <div className='completed-difficulties-container'>
+      <span className='difficulty easy'></span>
+      <span className='difficulty medium'></span>
+      <span className='difficulty hard'></span>
+    </div>
+  )
+}
