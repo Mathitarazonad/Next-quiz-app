6ade458ded4d20e5e5d36f9036d00a935d0472c3
MathiTarazonad <vladitarazona@gmail.com>
2023-01-17 14:34:03 -0300

    Created initial component for level selection button

---
+++ src/components/LevelButton.jsx
@@ -0,0 +1,11 @@
+import CompletedDifficulties from "./CompletedDifficulties";
+
+
+export default function LevelButton({children}) {
+  return (
+    <div className='single-level-button'>
+      {children}
+      <CompletedDifficulties />
+    </div>
+  )
+}
