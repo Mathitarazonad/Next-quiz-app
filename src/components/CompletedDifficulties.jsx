7d5efe8b03666a76bca957b848fc99e9abddb595
MathiTarazonad <vladitarazona@gmail.com>
2023-01-18 09:33:40 -0300

    Add classname completed based on which difficulties has the user completed, also added use client to enable hooks in this component

--- src/components/CompletedDifficulties.jsx
+++ src/components/CompletedDifficulties.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+"use client"
+import { LevelsContext } from "@/contexts/LevelsContext"
+import { useContext } from "react"
 
-export default function CompletedDifficulties() {
+export default function CompletedDifficulties({level}) {
+  const {difficultiesCompleted} = useContext(LevelsContext);
+  //0-None  1-Easy completed  2-Medium completed  3-Hard completed
+
   return (
     <div className='completed-difficulties-container'>
-      <span className='difficulty easy'></span>
-      <span className='difficulty medium'></span>
-      <span className='difficulty hard'></span>
+      <span className={difficultiesCompleted[level-1] === 1
+        ? 'difficulty easy completed' 
+        : 'difficulty easy'}></span>
+      <span className={difficultiesCompleted[level-1] === 2 
+        ? 'difficulty medium completed' 
+        : 'difficulty medium'}></span>
+      <span className={difficultiesCompleted[level-1] === 3 
+        ? 'difficulty hard completed' 
+        : 'difficulty hard'}></span>
     </div>
   )
 }
