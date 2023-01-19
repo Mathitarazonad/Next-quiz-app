98eb83c98a3a4907cffb33d93eee4362db017441
MathiTarazonad <vladitarazona@gmail.com>
2023-01-19 09:04:35 -0300

    Created a backbtn component to go back with path as a prop to reutilize it wherever

---
+++ src/components/BackButton.jsx
@@ -0,0 +1,10 @@
+import {IoMdArrowRoundBack} from 'react-icons/io';
+import Link from 'next/link';
+
+export default function BackButton({path}) {
+  return (
+    <Link className='back-btn' href={path}>
+      <IoMdArrowRoundBack />
+    </Link>
+  )
+}
