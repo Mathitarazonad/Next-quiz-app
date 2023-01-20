9f1dd68dc9b773b93286f3094138d02d9d40659e
MathiTarazonad <vladitarazona@gmail.com>
2023-01-20 09:09:41 -0300

    Modified function to avoid bug with lowercase characters that shows as they are not in the word even if the character was there but was uppercase

--- src/functions/wordFunctions.js
+++ src/functions/wordFunctions.js
@@ -3,7 +3,6 @@ export function checkCharacters (word, charPositions) {
   1-Char isn't in the word  
   2-Char is in the word but not in that position  
   3-Char is in the word and is in that position */
-  
   let resultArr = Array(word.length).fill('');
   let chrsUsed = [];
 
@@ -15,14 +14,14 @@ export function checkCharacters (word, charPositions) {
       chrsUsed.push(word[i]);
 
       //Check if there are characters repeated that are more than in the actual word
-      if (!IsCharacterAvailable(word, chrsUsed, word[i])) {
-        const repeatedIndexs = checkForRepeatedCharacters(word, word[i], resultArr, chrsUsed, chrsUsed.filter(chr => chr === word[i]).length - word.split('').filter(chr => chr === word[i]).length);
+      if (!IsCharacterAvailable(word, chrsUsed, word[i].toLowerCase())) {
+        const repeatedIndexs = checkForRepeatedCharacters(word, word[i].toLowerCase(), resultArr, chrsUsed, chrsUsed.filter(chr => chr === word[i].toLowerCase()).length - word.split('').filter(chr => chr === word[i].toLowerCase()).length);
 
         repeatedIndexs.forEach(i => {
           resultArr[i] = 1;
         })
       }
-    } else if (word.includes(currentChr) && IsCharacterAvailable(word, chrsUsed, currentChr)) {
+    } else if (word.toLowerCase().includes(currentChr) && IsCharacterAvailable(word, chrsUsed, currentChr)) {
       chrsUsed.push(currentChr);
       resultArr[i] = 2;
     } else {
@@ -46,5 +45,5 @@ function checkForRepeatedCharacters(word, chr, arrOfResults, usedCharacters, ext
 }
 
 function IsCharacterAvailable(word, chrsUsed, chr) {
-  return word.split('').filter(currentChr => currentChr === chr).length > chrsUsed.filter(currentChr => currentChr === chr).length;
+  return word.toLowerCase().split('').filter(currentChr => currentChr === chr).length > chrsUsed.filter(currentChr => currentChr === chr).length;
 }
