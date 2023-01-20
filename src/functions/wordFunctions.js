9283861e4e04bdcc4523f9c96694572a57250da3
MathiTarazonad <vladitarazona@gmail.com>
2023-01-20 08:58:00 -0300

    Modified function and added two complementaries to avoid bugs with repeated characters

--- src/functions/wordFunctions.js
+++ src/functions/wordFunctions.js
@@ -1,8 +1,50 @@
 export function checkCharacters (word, charPositions) {
-  //Return an array of: 1-Char isn't in the word  2-Char is in the word but not in that position  3-Char is in the word and is in that position
-  return charPositions.map((chr, i) => 
-    word[i] === chr ? 3 
-    : word.includes(chr) ? 2 
-    : 1
-  )
+/*Return an array of each position with its clue: 
+  1-Char isn't in the word  
+  2-Char is in the word but not in that position  
+  3-Char is in the word and is in that position */
+  
+  let resultArr = Array(word.length).fill('');
+  let chrsUsed = [];
+
+  for (let i = 0; i < word.length; i++) {
+    const currentChr = charPositions[i];
+
+    if (word[i] === currentChr){
+      resultArr[i] = 3;
+      chrsUsed.push(word[i]);
+
+      //Check if there are characters repeated that are more than in the actual word
+      if (!IsCharacterAvailable(word, chrsUsed, word[i])) {
+        const repeatedIndexs = checkForRepeatedCharacters(word, word[i], resultArr, chrsUsed, chrsUsed.filter(chr => chr === word[i]).length - word.split('').filter(chr => chr === word[i]).length);
+
+        repeatedIndexs.forEach(i => {
+          resultArr[i] = 1;
+        })
+      }
+    } else if (word.includes(currentChr) && IsCharacterAvailable(word, chrsUsed, currentChr)) {
+      chrsUsed.push(currentChr);
+      resultArr[i] = 2;
+    } else {
+      chrsUsed.push(currentChr)
+      resultArr[i] = 1;
+    }
+  }
+  return resultArr;
+}
+
+function checkForRepeatedCharacters(word, chr, arrOfResults, usedCharacters, extraChrs) {
+  const arrOfIndexs = [];
+
+  for (let i = 0; i < word.length && extraChrs !== 0; i++) {
+    if (usedCharacters[i] === chr && arrOfResults[i] !== 3) {
+      extraChrs--;
+      arrOfIndexs.push(i);
+    }
+  }
+  return arrOfIndexs;
+}
+
+function IsCharacterAvailable(word, chrsUsed, chr) {
+  return word.split('').filter(currentChr => currentChr === chr).length > chrsUsed.filter(currentChr => currentChr === chr).length;
 }
