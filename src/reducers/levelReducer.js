51d35f3e3104be71fab33b2d1ec40edfbddc0c12
MathiTarazonad <vladitarazona@gmail.com>
2023-01-27 15:12:10 -0300

    Change initialState name and the reducer actions

--- src/reducers/levelReducer.js
+++ src/reducers/levelReducer.js
@@ -1,31 +1,44 @@
-import levelsData from '@/data/levels.json'
+import levelsData from '@/data/levels.json';
 
-export const Levels = [
-]
+export const initialState = [];
 
 for (let i = 0; i < levelsData.length; i++) {
-  Levels.push({
-    'level' : i + 1,
-    'isCompleted' : false,
-    'completedDifficulties' : [] // 1-Easy  2-Normal  3-Hard
-  })
+  initialState.push({
+    level: i + 1,
+    isCompleted: false,
+    completedDifficulties: [], // 1-Easy  2-Normal  3-Hard
+  });
 }
 
 export const levelReducer = (state, action) => {
-  const {level, difficulty} = action;
-  const currentLevel = state[level-1];
+  const { level, difficulty } = action.payload;
+  const currentLevel = state[level - 1];
 
-
-  switch(action) {
+  switch (action.type) {
     case 'ADD_COMPLETED_DIFFICULTY': {
-      currentLevel.completedDifficulties.push(difficulty);
-      break;
-    } 
-    case 'SET_COMPLETED_LEVEL': {
-      currentLevel.isCompleted = true;
-      break;
+      const newDifficulties = [
+        ...currentLevel.completedDifficulties,
+        difficulty,
+      ];
+      state.map((lvl, index) =>
+        index === level - 1 
+          ? {
+              level: lvl.level,
+              isCompleted: lvl.isCompleted,
+              completedDifficulties: newDifficulties,
+            }
+          : lvl
+      );
     }
-      
+    case 'SET_COMPLETED_LEVEL': {
+      const newState = !currentLevel.isCompleted;
+      return state.map((lvl, index) => index === level-1 ? {
+        level: lvl.level,
+        isCompleted: newState,
+        completedDifficulties: newDifficulties,
+      } : lvl)
+    }
+    default:
+      return state;
   }
-}
-
+};
