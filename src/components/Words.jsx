a5e8af1fc3000fc7a270abdd71449482807f6001
MathiTarazonad <vladitarazona@gmail.com>
2023-01-31 09:17:28 -0300

    Wrapped all words with the words provider

--- src/components/Words.jsx
+++ src/components/Words.jsx
@@ -1,19 +1,23 @@
+'use client'
 import SingleWord from './SingleWord';
 import levelsData from '@/data/levels.json';
+import WordsProvider from '@/contexts/WordsContext';
 
 export default function Words({ level, difficulty }) {
   const words = levelsData[level - 1][difficulty];
 
   return (
-    <div className="words-container">
-      {words.map((word) => (
-        <SingleWord
-          word={word}
-          key={word}
-          level={level}
-          difficulty={difficulty}
-        />
-      ))}
-    </div>
+    <WordsProvider>
+      <div className="words-container">
+        {words.map((word) => (
+          <SingleWord
+            word={word}
+            key={word}
+            level={level}
+            difficulty={difficulty}
+          />
+        ))}
+      </div>
+    </WordsProvider>
   );
 }
