444efcff6487a2bc76e0f46c832a8e00663dfc8d
MathiTarazonad <vladitarazona@gmail.com>
2023-01-31 09:16:52 -0300

    Updated component with the new level context for the completed difficulties

--- src/components/CompletedDifficulties.jsx
+++ src/components/CompletedDifficulties.jsx
@@ -3,18 +3,19 @@ import { LevelsContext } from "@/contexts/LevelsContext"
 import { useContext } from "react"
 
 export default function CompletedDifficulties({level}) {
-  const {difficultiesCompleted} = useContext(LevelsContext);
+  const {levels} = useContext(LevelsContext)
+  const {completedDifficulties} = levels[level-1]
   //0-None  1-Easy completed  2-Medium completed  3-Hard completed
 
   return (
     <div className='completed-difficulties-container'>
-      <span className={difficultiesCompleted[level-1] === 1
+      <span className={completedDifficulties.includes(1)
         ? 'difficulty easy completed' 
         : 'difficulty easy'}></span>
-      <span className={difficultiesCompleted[level-1] === 2 
+      <span className={completedDifficulties.includes(2) 
         ? 'difficulty medium completed' 
         : 'difficulty medium'}></span>
-      <span className={difficultiesCompleted[level-1] === 3 
+      <span className={completedDifficulties.includes(3)
         ? 'difficulty hard completed' 
         : 'difficulty hard'}></span>
     </div>
