8622ce6603adee329889b77d1d987d9539ece8c2
MathiTarazonad <vladitarazona@gmail.com>
2023-02-03 11:18:09 -0300

    Created component for inputs

---
+++ src/components/CharacterInput.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function CharacterInput({characters, inputRef, index, handleChange, handleKey, completed, charClues}) {
+  return (
+    <>
+      <input minLength={1} maxLength={1} 
+         disabled={completed ? true : false}
+        ref={inputRef}
+        style={{width: 50, height:50}} //Temporal styles
+        value={characters[index]} 
+        onChange={(e) => handleChange(e, index)}
+        onKeyUp={(e) => handleKey(e, index)}
+        className={charClues[index] === 1 ? 'single-input none' 
+        : charClues[index] === 2 ? 'single-input in-word' 
+        : charClues[index] === 3 ? 'single-input correct' 
+        : 'single-input'}
+      />
+      <style jsx>{`
+        .single-input.none {
+          background-color: gray;
+        }
+        .single-input.in-word {
+          background-color: yellow;
+        }
+        .single-input.correct {
+          background-color: green;
+        }
+      `}
+      </style>
+    </>
+  )
+}
