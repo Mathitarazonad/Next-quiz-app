37ce450842520b4f1b74681156869dabc1369394
MathiTarazonad <vladitarazona@gmail.com>
2023-02-13 15:03:44 -0300

    Handle auth state and redirect where it corresponds with next router and exported custom hook to call the usecontext of usercontext

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,12 +1,17 @@
 'use client'
-import { createContext,useState, useEffect } from 'react';
+import { createContext,useState, useEffect, useContext } from 'react';
 import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import { auth } from '@firebase/firebaseApp';
+import { useRouter } from 'next/navigation';
 
 export const UserContext = createContext();
+export function UserAuth () {
+  return useContext(UserContext);
+}
 
 export default function UserProvider({children}) {
   const [currentUser, setCurrentUser] = useState(null);
+  const router = useRouter();
 
   const signUp = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
@@ -23,11 +28,17 @@ export default function UserProvider({children}) {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setCurrentUser(user);
+      if(!user) {
+        router.push('/login')
+      }
+      if(user) {
+        router.push('/')
+      }
     });
     return () => {
       unsubscribe();
     };
-  },[])
+  },[router])
 
   const value = {
     currentUser,
