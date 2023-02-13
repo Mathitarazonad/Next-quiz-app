a229fce3dc1916ffc38605446c3a003a1b5de4c8
MathiTarazonad <vladitarazona@gmail.com>
2023-02-13 15:05:04 -0300

    Edited padding top

--- src/components/RegisterComponents/Alert.jsx
+++ src/components/RegisterComponents/Alert.jsx
@@ -2,7 +2,7 @@ import { IoMdClose } from 'react-icons/io';
 
 export default function Alert ({message, handleCloseError,}) {
   return (
-    <div className='flex justify-between items-center bg-red-300 text-red-700 border-[.5px] border-red-700 font-bold w-full px-5 py-4 rounded-lg text-xs relative'>
+    <div className='flex justify-between items-center bg-red-300 text-red-700 border-[.5px] border-red-700 font-bold w-full px-5 py-3 rounded-lg text-xs relative'>
       {message}
       <IoMdClose className='text-sm' onClick={handleCloseError}/>
     </div>
