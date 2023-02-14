99ab3a1e3219a7198d90ca05bbf34c906ee992ca
MathiTarazonad <vladitarazona@gmail.com>
2023-02-14 15:35:37 -0300

    Created function for generate the static parameters for each level with each difficulty

--- src/app/levels/[level]/[difficulty]/page.jsx
+++ src/app/levels/[level]/[difficulty]/page.jsx
@@ -1,13 +1,22 @@
-import BackButton from '@/components/BackButton';
-import Words from '@/components/GameComponents/Words';
+import BackButton from '@/components/BackButton'
+import Words from '@/components/GameComponents/Words'
+import levelsData from '@/data/levels.json'
 
-export default function page({ params }) {
-  const { level, difficulty } = params;
+export default function Page ({ params }) {
+  const { level, difficulty } = params
 
   return (
     <div className='level-container'>
       <BackButton path={`/levels/${level}`} />
       <Words level={parseInt(level)} difficulty={difficulty} />
     </div>
-  );
+  )
+}
+
+export async function generateStaticParams () {
+  const difficulties = ['easy', 'medium', 'hard']
+  return difficulties.map(diff => levelsData.map(lvl => ({
+    level: lvl.level.toString(),
+    difficulty: diff
+  }))).flat()
 }
