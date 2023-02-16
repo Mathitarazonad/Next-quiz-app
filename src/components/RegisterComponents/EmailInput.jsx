7bf09b20673b8b4291ca32f13df8efb244e5db54
MathiTarazonad <vladitarazona@gmail.com>
2023-02-16 15:19:51 -0300

    Changed icon for email field

--- src/components/RegisterComponents/EmailInput.jsx
+++ src/components/RegisterComponents/EmailInput.jsx
@@ -1,11 +1,11 @@
-import { FaUser } from 'react-icons/fa'
+import { HiOutlineMail } from 'react-icons/hi'
 
 export default function EmailInput () {
   return (
     <div className='form-section'>
       <h2 className='input-title'>Enter an Email</h2>
       <div className='input-container'>
-        <FaUser className='input-icon' />
+        <HiOutlineMail className='input-icon' />
         <input
           className='input'
           type='email'
