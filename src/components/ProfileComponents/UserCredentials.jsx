323db06318edc6efe0d76e987fd4385baea3838a
MathiTarazonad <vladitarazona@gmail.com>
2023-02-17 14:45:07 -0300

    Created components to render all types of credentials changes

---
+++ src/components/ProfileComponents/UserCredentials.jsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+export default function UserCredentials() {
+  return (
+    <div className='flex flex-col gap-2 w-full'>
+      <h2 className='text-semi-dark-violet text-lg font-bold text-center'>Credentials Change</h2>
+      <Link href='/change-email' className='rounded-md bg-violet-500 bg-opacity-80 p-2 text-xs font-semibold text-white text-center flex-1'>
+        Change email
+      </Link>
+      <Link href='/change-password' className='rounded-md bg-violet-500 bg-opacity-80 p-2 text-xs font-semibold text-white text-center flex-1'>
+        Change password
+      </Link>
+    </div>
+  )
+}
