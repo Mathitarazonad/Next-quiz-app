f7203881176dab7c70f3038a1e62a95fbf8fd662
MathiTarazonad <vladitarazona@gmail.com>
2023-02-20 14:47:54 -0300

    Added a optional chaining to avoid a bug

--- src/app/account/page.jsx
+++ src/app/account/page.jsx
@@ -1,21 +1,25 @@
 'use client'
-import { BiArrowBack } from 'react-icons/bi'
-import { UserAuth } from '@/contexts/UserContext'
+import { useUser } from '@/contexts/UserContext'
 import Image from 'next/image'
 import UserCompletedLevels from '@/components/ProfileComponents/UserCompletedLevels'
 import UserCredentials from '@/components/ProfileComponents/UserCredentials'
 import ProtectedRoutes from '@/components/RegisterComponents/ProtectedRoutes'
 
 export default function AccountPage() {
-  const { currentUser } = UserAuth()
+  const { currentUser } = useUser()
 
   return (
     <ProtectedRoutes path='/login'>
       <div className='min-h-screen max-w-md flex items-center mx-auto px-10'>
         <div className='rounded-xl bg-white flex flex-col justify-center items-center py-7 px-7 md:px-10 lg:px-12 gap-7 w-full shadow-lg'>
-          <Image src='/user-photo.png' width={130} height={130} alt='User Profile Photo' />
+          <Image
+            src='/user-photo.png'
+            width={130}
+            height={130}
+            alt='User Profile Photo'
+          />
           <h1 className='text-dark-violet-title font-bold text-2xl -mt-3 text-center w-full'>
-            {currentUser.displayName + "'s"} Profile
+            {currentUser?.displayName + "'s"} Profile
           </h1>
           <UserCompletedLevels />
           <UserCredentials />
