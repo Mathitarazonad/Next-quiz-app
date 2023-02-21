cddc60710eb81956f44f1bc26081789b2cdf20f1
MathiTarazonad <vladitarazona@gmail.com>
2023-02-21 11:46:37 -0300

    Created button to go to user profile page

---
+++ src/components/ProfileComponents/ProfileButton.jsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { AiOutlineUser } from 'react-icons/ai'
+
+export default function ProfileButton() {
+  return (
+    <Link href='/account' className='absolute top-5 left-5 hover:scale-105 duration-300 rounded-full border-[4px] border-dark-violet-title'>
+      <AiOutlineUser className='fill-dark-violet-title text-[43px]' />
+    </Link>
+  )
+}
