bc3114521d8af087adfd7e55a70e4a7dc83d0778
MathiTarazonad <vladitarazona@gmail.com>
2023-02-21 11:47:28 -0300

    Added buttons for go back and user coins

--- src/app/account/page.jsx
+++ src/app/account/page.jsx
@@ -4,14 +4,18 @@ import Image from 'next/image'
 import UserCompletedLevels from '@/components/ProfileComponents/UserCompletedLevels'
 import UserCredentials from '@/components/ProfileComponents/UserCredentials'
 import ProtectedRoutes from '@/components/RegisterComponents/ProtectedRoutes'
+import UserCoins from '@/components/MenuComponents/UserCoins'
+import BackButton from '@/components/BackButton'
 
 export default function AccountPage() {
   const { currentUser } = useUser()
 
   return (
     <ProtectedRoutes path='/login'>
-      <div className='min-h-screen max-w-md flex items-center mx-auto px-10'>
-        <div className='rounded-xl bg-white flex flex-col justify-center items-center py-7 px-7 md:px-10 lg:px-12 gap-7 w-full shadow-lg'>
+      <div className='min-h-screen max-w-md flex items-center mx-auto px-7 md:px-10'>
+        <div className='rounded-xl bg-white flex flex-col justify-center items-center py-7 px-7 md:px-10 lg:px-12 gap-7 w-full shadow-lg relative'>
+          <BackButton />
+          <UserCoins />
           <Image
             src='/user-photo.png'
             width={130}
