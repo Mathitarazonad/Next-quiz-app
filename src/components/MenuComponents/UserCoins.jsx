4ecaffe5cdc382a24890a1f4d9f3818920579dad
MathiTarazonad <vladitarazona@gmail.com>
2023-03-01 12:01:29 -0300

    Added functionality to add coins when user enter a correct word or gain a reward, and added default animation to the coin without controlling it with a state

--- src/components/MenuComponents/UserCoins.jsx
+++ src/components/MenuComponents/UserCoins.jsx
@@ -2,12 +2,11 @@
 import { useCoins } from '@/contexts/CoinsContext'
 
 export default function UserCoins() {
-  const { coins, animate } = useCoins()
-  const defaultStyles = 'rounded-full border-[5px] border-coin-border border-opacity-60 bg-coin-background w-[50px] h-[50px] text-2xl flex items-center justify-center text-white tracking-wide font-bold overflow-hidden relative after:absolute after:content-[""] after:top-0 after:-left-7 after:w-1/2 after:h-full after:skew-x-[-30deg] after:bg-gray-200 after:bg-opacity-30 hover:after:left-12 after:duration-[300ms] select-none'
+  const { coins } = useCoins()
 
   return (
     <div className='flex flex-col items-center gap-1 absolute top-5 right-5'>
-      <p className={animate ? defaultStyles + ' after:animate-coin-shine animate-coin-pulse' : defaultStyles} key={Math.random()}>
+      <p className='rounded-full border-[5px] border-coin-border border-opacity-60 bg-coin-background w-[50px] h-[50px] text-2xl flex items-center justify-center text-white tracking-wide font-bold overflow-hidden relative after:absolute after:content-[""] after:top-0 after:-left-7 after:w-1/2 after:h-full after:skew-x-[-30deg] after:bg-gray-200 after:bg-opacity-30 hover:after:left-12 after:duration-[300ms] select-none after:animate-coin-shine animate-coin-pulse' key={Math.random()}>
         {coins}
       </p>
     </div>
--- src/contexts/CoinsContext.jsx
+++ src/contexts/CoinsContext.jsx
@@ -1,4 +1,6 @@
 'use client'
+
+import { usePathname } from 'next/navigation'
 import { createContext, useContext, useEffect, useState } from 'react'
 
 export const CoinsContext = createContext()
@@ -6,26 +8,37 @@ export const useCoins = () => useContext(CoinsContext)
 
 export default function CoinsProvider ({ children }) {
   const [coins, setCoins] = useState(0)
-  const [coinsToAdd, setCoinsToAdd] = useState(0)
-  const [animate, setAnimate] = useState(false)
+  const [coinsToAdd, setCoinsToAdd] = useState(0) // State for animation when coins are added
+  const [coinsGained, setCoinsGained] = useState(0) // State to control the coins gained and rest them if user leaves the level
+  const path = usePathname()
+
+  const value = {
+    coins, setCoins,
+    coinsToAdd, setCoinsToAdd,
+    coinsGained, setCoinsGained
+  }
 
   useEffect(() => {
     if (coinsToAdd !== 0) {
-      setAnimate(true)
       const timer = setInterval(() => {
         setCoins(coins + 1)
         setCoinsToAdd(coinsToAdd - 1)
         new Audio('/sounds/coinGain.wav').play()
-      }, 750)
-
+      }, 300)
       return () => clearInterval(timer)
-    } else {
-      setAnimate(false)
     }
   }, [coinsToAdd, coins])
 
+  useEffect(() => {
+    // If user leaves a level without completing it, then rest the coins that gained in that level
+    if (coinsGained > 0) {
+      setCoins(prevState => prevState - coinsGained)
+      setCoinsGained(0)
+    }
+  }, [path])
+
   return (
-    <CoinsContext.Provider value={{ coins, setCoins, coinsToAdd, setCoinsToAdd, animate }}>
+    <CoinsContext.Provider value={value}>
       {children}
     </CoinsContext.Provider>
   )
