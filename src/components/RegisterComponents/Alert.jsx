04f317603e56eb8f038817aa6388395747bc6b39
MathiTarazonad <vladitarazona@gmail.com>
2023-03-03 14:45:24 -0300

    Added cursor pointer to the error message close

--- src/components/RegisterComponents/Alert.jsx
+++ src/components/RegisterComponents/Alert.jsx
@@ -4,7 +4,7 @@ export default function Alert ({ message, handleCloseError }) {
   return (
     <div className='flex justify-between items-center bg-red-300 text-red-700 border-[.5px] border-red-700 font-bold w-full px-5 py-3 rounded-lg text-xs relative'>
       {message}
-      <IoMdClose className='text-sm' onClick={handleCloseError} />
+      <IoMdClose className='text-sm cursor-pointer' onClick={handleCloseError} />
     </div>
   )
 }
