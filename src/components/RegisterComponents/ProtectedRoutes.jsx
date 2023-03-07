a673a127b6ec4b8065d4a6918f55745615284e62
MathiTarazonad <vladitarazona@gmail.com>
2023-03-07 09:02:16 -0300

    Minor fix just to avoid the useEffect being callled multiple times

--- src/components/RegisterComponents/ProtectedRoutes.jsx
+++ src/components/RegisterComponents/ProtectedRoutes.jsx
@@ -35,7 +35,7 @@ export default function ProtectedRoutes({ children, path, authentication = false
         })
       }
     }
-  }, [currentUser, userChecked])
+  }, [userChecked])
 
   return (
     <>
