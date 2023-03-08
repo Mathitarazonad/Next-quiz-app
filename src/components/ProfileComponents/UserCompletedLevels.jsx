7bfb876913be15adbfa0732314684dfdcd611c3c
MathiTarazonad <vladitarazona@gmail.com>
2023-03-08 14:17:36 -0300

    Fixed style: added flex wrap

--- src/components/ProfileComponents/UserCompletedLevels.jsx
+++ src/components/ProfileComponents/UserCompletedLevels.jsx
@@ -8,7 +8,7 @@ export default function UserCompletedLevels() {
   return (
     <div className='flex flex-col justify-center gap-2'>
       <h2 className='w-full text-center text-lg font-bold text-semi-dark-violet'>Completed Levels</h2>
-      <div className='flex justify-center gap-2'>
+      <div className='flex justify-center gap-2 flex-wrap'>
         {completedLevels.length > 0
           ? completedLevels.map(lvl => <LevelCompleted key={lvl} level={lvl.level} />)
           : <p className='text-xs text-semi-dark-violet text-center font-semibold'>You didn't completed any levels yet!</p>}
