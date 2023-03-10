81c2bcaf452b00ceff1632457a1f2e98f2497f5d
MathiTarazonad <vladitarazona@gmail.com>
2023-03-10 10:58:02 -0300

    Renamed words context to CurrentLevelContext and added some states to manage the abilities in the future

---
+++ src/contexts/CurrentLevelContext.jsx
@@ -0,0 +1,24 @@
+'use client'
+import { createContext, useContext, useState } from 'react'
+
+export const CurrentLevelContext = createContext()
+export const useCurrentLevel = () => useContext(CurrentLevelContext)
+
+export default function CurrentLevelProvider ({ children }) {
+  const [completedWords, setCompletedWords] = useState(Array(5).fill(false))
+  // 1-Reveal half of the word  2-Reveal a random word  3-Reveal the current word
+  const [abilityToUse, setAbilityToUse] = useState(0)
+  const [currentWord, setCurrentWord] = useState(null)
+
+  const value = {
+    completedWords, setCompletedWords,
+    abilityToUse, setAbilityToUse,
+    currentWord, setCurrentWord
+  }
+
+  return (
+    <CurrentLevelContext.Provider value={value}>
+      {children}
+    </CurrentLevelContext.Provider>
+  )
+}
