f2e15b7d4924cfe377689e1d28994fe48d2b8625
MathiTarazonad <vladitarazona@gmail.com>
2023-03-10 10:59:02 -0300

    Wrapped all the page with the currentLevelProvier

--- src/app/levels/[level]/[difficulty]/page.jsx
+++ src/app/levels/[level]/[difficulty]/page.jsx
@@ -3,20 +3,23 @@ import Words from '@/components/GameComponents/Words'
 import levelsData from '@/data/levels.js'
 import UserCoins from '@/components/MenuComponents/UserCoins'
 import IconsPair from '@/components/MenuComponents/IconsPair'
+import CurrentLevelProvider from '@/contexts/CurrentLevelContext'
 
 export default function Page({ params }) {
   const { level, difficulty } = params
 
   return (
     <div className='card gap-2 p-7'>
-      <IconsPair>
-        <BackButton />
-        <UserCoins />
-      </IconsPair>
-      <h1 className='text-4xl text-dark-violet-title font-bold'>Level {level}</h1>
-      <h2 className='text-2xl font-bold text-semi-dark-violet capitalize mb-1'>Hint: {levelsData[level - 1].topic}</h2>
-      <h3 className='text-xl font-bold text-semi-dark-violet capitalize mb-1'>{difficulty}</h3>
-      <Words level={parseInt(level)} difficulty={difficulty} />
+      <CurrentLevelProvider>
+        <IconsPair>
+          <BackButton />
+          <UserCoins />
+        </IconsPair>
+        <h1 className='text-4xl text-dark-violet-title font-bold'>Level {level}</h1>
+        <h2 className='text-2xl font-bold text-semi-dark-violet capitalize mb-1'>Hint: {levelsData[level - 1].topic}</h2>
+        <h3 className='text-xl font-bold text-semi-dark-violet capitalize mb-1'>{difficulty}</h3>
+        <Words level={parseInt(level)} difficulty={difficulty} />
+      </CurrentLevelProvider>
     </div>
   )
 }
