60c464bcb42330d554c47088c909f39dbcec9134
MathiTarazonad <vladitarazona@gmail.com>
2023-03-13 09:19:59 -0300

    Deleted the new level unlocked component, added abilities and passed wordIndex to singleWord component

--- src/components/GameComponents/Words.jsx
+++ src/components/GameComponents/Words.jsx
@@ -1,32 +1,34 @@
 'use client'
 import SingleWord from './SingleWord'
 import levelsData from '@/data/levels.js'
-import WordsProvider from '@/contexts/WordsContext'
 import { useLevels } from '@/contexts/LevelsContext'
-import LevelUnlocked from '../LevelUnlocked'
 import DifficultyReward from './Rewards'
+import Abilities from './Abilities'
 
 export default function Words ({ level, difficulty }) {
   const words = levelsData[level - 1][difficulty]
-  const { newLevelUnlocked, difficultyPassed } = useLevels()
+  const { difficultyPassed } = useLevels()
 
   return (
-    <WordsProvider>
-      <div className={words[0].length > 6 ? 'flex flex-col gap-2' : 'flex flex-col gap-3'}>
-        {words.map((word) => (
-          <SingleWord
-            word={word}
-            key={word}
-            level={level}
-            difficulty={
-              difficulty === 'easy' ? 1 : difficulty === 'medium' ? 2 : 3
-            }
-          />
-        ))}
-        {newLevelUnlocked && <LevelUnlocked />}{' '}
-        {/* Alert the user that a new level has been unlocked */}
+    <>
+      <div className='flex flex-col gap-3 w-full'>
+        <div className={words[0].length > 6 ? 'flex flex-col gap-2 items-center' : 'flex flex-col gap-3 items-center'}>
+          {words.map((word, index) => (
+            <SingleWord
+              word={word}
+              wordIndex={index}
+              key={word}
+              level={level}
+              difficulty={
+                difficulty === 'easy' ? 1 : difficulty === 'medium' ? 2 : 3
+              }
+            />
+          ))}
+          {/* Alert the user that a new level has been unlocked */}
+        </div>
+        <Abilities level={level} difficulty={difficulty === 'easy' ? 1 : difficulty === 'medium' ? 2 : 3} />
       </div>
       {difficultyPassed && <DifficultyReward level={level} difficulty={difficulty === 'easy' ? 1 : difficulty === 'medium' ? 2 : 3} />}
-    </WordsProvider>
+    </>
   )
 }
