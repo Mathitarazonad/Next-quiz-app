dbfb20eeb8fc2fb704c2165ad50c38c6f5a61178
MathiTarazonad <vladitarazona@gmail.com>
2023-03-14 11:05:54 -0300

    Fixed some styles

--- src/components/GameComponents/CharacterInput.jsx
+++ src/components/GameComponents/CharacterInput.jsx
@@ -6,11 +6,11 @@ export default function CharacterInput ({ characters, inputRefs, index, wordInde
       : characters.length > 6 ? `w-8 h-8 ${defaultStyles} text-base md:w-[41px] md:h-[41px] md:text-lg`
         : `w-[45px] h-[45px] ${defaultStyles} lg:w-[50px] lg:h-[50px]`
 
-  const incorrectClue = ' bg-red-500 bg-opacity-20 border-red-500 border-opacity-60 text-red-500 text-opacity-60 focus:border-red-500 focus:border-opacity-80 hover:border-red-500 hover:border-opacity-90'
+  const incorrectClue = ' bg-red-500 bg-opacity-20 border-red-500 border-opacity-60 text-red-500 text-opacity-60 focus:border-red-500 focus:border-opacity-90 hover:border-red-500 hover:border-opacity-90'
 
   const inWordClue = ' bg-yellow-100 border-yellow-700 border-opacity-40 text-yellow-700 text-opacity-40 focus:border-yellow-800 focus:border-opacity-50 hover:border-yellow-800 hover:border-opacity-60'
 
-  const correctClue = ' bg-green-100 border-green-700 border-opacity-50 text-green-700 text-opacity-50 focus:border-green-700 focus:border-opacity-70 hover:border-green-700 hover:border-opacity-70'
+  const correctClue = ' bg-green-100 border-green-700 border-opacity-50 text-green-700 text-opacity-50 hover:border-green-700 hover:border-opacity-50'
 
   const wordNotExistClue = ' bg-semi-dark-violet bg-opacity-10 border-semi-dark-violet border-opacity-50 text-gray-500 text-opacity-70 focus:border-semi-dark-violet focus:border-opacity-70 hover:border-semi-dark-violet hover:border-opacity-70'
 
