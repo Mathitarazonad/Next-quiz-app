0449523f45e0eae22ae7bb78de4f262c1bb7b0d7
MathiTarazonad <vladitarazona@gmail.com>
2023-03-14 11:16:31 -0300

    Added hover effects

--- src/components/ProfileComponents/UserCredentials.jsx
+++ src/components/ProfileComponents/UserCredentials.jsx
@@ -4,10 +4,10 @@ export default function UserCredentials() {
   return (
     <div className='flex flex-col gap-2 w-full'>
       <h2 className='text-semi-dark-violet text-lg font-bold text-center'>Credentials Change</h2>
-      <Link href='/change-email' className='rounded-md bg-violet-500 bg-opacity-80 p-2 text-xs font-semibold text-white text-center flex-1'>
+      <Link href='/change-email' className='rounded-md bg-violet-500 bg-opacity-80 p-2 text-xs font-semibold text-white text-center flex-1 hover:bg-opacity-70'>
         Change email
       </Link>
-      <Link href='/change-password' className='rounded-md bg-violet-500 bg-opacity-80 p-2 text-xs font-semibold text-white text-center flex-1'>
+      <Link href='/change-password' className='rounded-md bg-violet-500 bg-opacity-80 p-2 text-xs font-semibold text-white text-center flex-1 hover:bg-opacity-70'>
         Change password
       </Link>
     </div>
