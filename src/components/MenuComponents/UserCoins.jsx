c7b07481427e164f2f30a2e05f6a97cb44160fd7
MathiTarazonad <vladitarazona@gmail.com>
2023-03-14 11:20:18 -0300

    Fixed some styles

--- src/components/MenuComponents/UserCoins.jsx
+++ src/components/MenuComponents/UserCoins.jsx
@@ -6,7 +6,7 @@ export default function UserCoins() {
 
   return (
     <div className='flex flex-col items-center gap-1'>
-      <p className='rounded-full border-[5px] border-coin-border border-opacity-60 bg-coin-background w-[45px] h-[45px] text-xl flex items-center justify-center text-white tracking-wide font-bold overflow-hidden relative after:absolute after:content-[""] after:top-0 after:-left-7 after:w-1/2 after:h-full after:skew-x-[-30deg] after:bg-gray-200 after:bg-opacity-30 select-none after:animate-coin-shine animate-coin-pulse' key={Math.random()}>
+      <p className='rounded-full border-[6.5px] border-coin-border border-opacity-60 bg-coin-background w-[55px] h-[55px] text-xl flex items-center justify-center text-white tracking-wide font-bold overflow-hidden relative after:absolute after:content-[""] after:top-0 after:-left-7 after:w-1/2 after:h-full after:skew-x-[-30deg] after:bg-gray-200 after:bg-opacity-30 select-none after:animate-coin-shine animate-coin-pulse' key={Math.random()}>
         {coins}
       </p>
     </div>
