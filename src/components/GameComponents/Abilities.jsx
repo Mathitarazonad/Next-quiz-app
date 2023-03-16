b453f6a960f6bcd173279abc26a91b9277822927
MathiTarazonad <vladitarazona@gmail.com>
2023-03-16 09:56:50 -0300

    Changed ability description

--- src/components/GameComponents/Abilities.jsx
+++ src/components/GameComponents/Abilities.jsx
@@ -31,7 +31,7 @@ export default function Abilities({ level, difficulty }) {
 
   return (
     <div className='flex justify-between items-start w-full'>
-      <SingleHability cost={5} abilityDescription='Reveal half characters' ability={1} handleAbility={handleAbility} />
+      <SingleHability cost={5} abilityDescription='Reveal some characters' ability={1} handleAbility={handleAbility} />
       <SingleHability cost={10} abilityDescription='Reveal random word' ability={2} handleAbility={handleAbility} />
       <SingleHability cost={15} abilityDescription='Reveal current word' ability={3} handleAbility={handleAbility} />
     </div>
