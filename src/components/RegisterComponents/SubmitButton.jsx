1df66f7d3a188f6fa71a61ebb3ffd9d7aa7b3f11
MathiTarazonad <vladitarazona@gmail.com>
2023-03-17 15:44:27 -0300

    Added prop to disable the submit button, by default it is false

--- src/components/RegisterComponents/SubmitButton.jsx
+++ src/components/RegisterComponents/SubmitButton.jsx
@@ -1,7 +1,8 @@
-export default function SubmitButton ({ children }) {
+export default function SubmitButton ({ children, isDisabled = false }) {
   return (
     <button
       type='submit'
+      disabled={isDisabled}
       className='bg-violet-500 bg-opacity-90 w-full py-[6px] rounded-md text-white text-sm font-semibold hover:bg-violet-400 duration-300'
     >
       {children}
