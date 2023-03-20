6c18bbd2e40c0fdd87479b983fd3cae0913f68c8
MathiTarazonad <vladitarazona@gmail.com>
2023-03-20 10:53:33 -0300

    Added pointer cursor to the submit button

--- src/components/RegisterComponents/SubmitButton.jsx
+++ src/components/RegisterComponents/SubmitButton.jsx
@@ -3,7 +3,7 @@ export default function SubmitButton ({ children, isDisabled = false }) {
     <button
       type='submit'
       disabled={isDisabled}
-      className='bg-violet-500 bg-opacity-90 w-full py-[6px] rounded-md text-white text-sm font-semibold hover:bg-violet-400 duration-300'
+      className='bg-violet-500 bg-opacity-90 w-full py-[6px] rounded-md text-white text-sm font-semibold hover:bg-violet-400 duration-300 cursor-pointer'
     >
       {children}
     </button>
