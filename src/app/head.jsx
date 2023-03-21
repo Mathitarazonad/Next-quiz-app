951ab834ce06d1c73a346ca25be3b07bc297aebe
MathiTarazonad <vladitarazona@gmail.com>
2023-03-21 09:32:01 -0300

    Changed title and description

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -1,9 +1,9 @@
 export default function Head () {
   return (
     <>
-      <title>Next Quiz App</title>
+      <title>Next 13 - Quiz App</title>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
-      <meta name='description' content='A word quiz app created with Next' />
+      <meta name='description' content='A word quiz app created with Next 13' />
       <link rel='icon' href='/favicon.ico' />
     </>
   )
